61614fb6da643768177fe9b4aba186faf7e9a9f1
15695947865@139.com <2695929724@qq.com>
2018-04-24 10:07:53 +0800

    1.增加跳转到指定目录

--- app/src/main/java/com/szhrnet/taoqiapp/utils/IntentUtils.java
+++ app/src/main/java/com/szhrnet/taoqiapp/utils/IntentUtils.java
@@ -3,10 +3,12 @@ package com.szhrnet.taoqiapp.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.szhrnet.taoqiapp.R;
 
+import java.io.File;
 import java.util.Set;
 
 /**
@@ -18,107 +20,213 @@ import java.util.Set;
  */
 public class IntentUtils {
 
-	public static void gotoActivity(Context context, Class<?> gotoClass ) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		context.startActivity(intent);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivityToTopAndFinish(Context context, Class<?> gotoClass) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		context.startActivity(intent);
-		((Activity) context).finish();
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivityToTopAndFinish(Context context, Class<?> gotoClass , Bundle bundle) {
-		Intent intent = new Intent();
-		intent.putExtras(bundle);
-		intent.setClass(context, gotoClass);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		context.startActivity(intent);
-		((Activity) context).finish();
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivity(Context context, Class<?> gotoClass, Bundle bundle) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.putExtras(bundle);
-		context.startActivity(intent);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivityForResult(Context context, Class<?> gotoClass, int requestCode) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		((Activity) context).startActivityForResult(intent, requestCode);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivityForResult(Context context, Class<?> gotoClass, Bundle bundle, int requestCode) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.putExtras(bundle);
-		((Activity) context).startActivityForResult(intent, requestCode);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	public static void gotoActivityAndFinish(Context context, Class<?> gotoClass) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		context.startActivity(intent);
-		((Activity) context).finish();
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
+    public static void gotoActivity(Context context, Class<?> gotoClass) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        context.startActivity(intent);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
 
-	public static void gotoActivityAndFinish(Context context, Class<?> gotoClass, Bundle bundle) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.putExtras(bundle);
-		context.startActivity(intent);
-		((Activity) context).finish();
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
+    public static void gotoActivityToTopAndFinish(Context context, Class<?> gotoClass) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
 
-	public static void gotoActivityToTop(Context context, Class<?> gotoClass) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		context.startActivity(intent);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
+    public static void gotoActivityToTopAndFinish(Context context, Class<?> gotoClass, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.putExtras(bundle);
+        intent.setClass(context, gotoClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
 
-	public static void gotoActivityToTop(Context context, Class<?> gotoClass, Bundle bundle) {
-		Intent intent = new Intent();
-		intent.setClass(context, gotoClass);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		intent.putExtras(bundle);
-		context.startActivity(intent);
-		((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
-	}
-	
-	/**
-	 * @updateTime 2017.2.10
-	 * @createAuthor Zou Juequn
-	 */
-	public static String getIntentInfo(Intent intent){
-		StringBuilder builder = new StringBuilder();
-		Bundle bd = intent.getExtras();
-		Set<String> set = bd.keySet();
-		for(String key:set){
-			builder.append("[");
-			builder.append(key+":");
-			builder.append(bd.get(key));
-			builder.append("]");
-		}
-		return builder.toString();
-	}
-	
+    public static void gotoActivity(Context context, Class<?> gotoClass, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+    public static void gotoActivityForResult(Context context, Class<?> gotoClass, int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        ((Activity) context).startActivityForResult(intent, requestCode);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+    public static void gotoActivityForResult(Context context, Class<?> gotoClass, Bundle bundle, int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.putExtras(bundle);
+        ((Activity) context).startActivityForResult(intent, requestCode);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+    public static void gotoActivityAndFinish(Context context, Class<?> gotoClass) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+
+    public static void gotoActivityAndFinish(Context context, Class<?> gotoClass, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+
+    public static void gotoActivityToTop(Context context, Class<?> gotoClass) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+
+    public static void gotoActivityToTop(Context context, Class<?> gotoClass, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(context, gotoClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+        ((Activity) context).overridePendingTransition(R.anim.enter_exit, R.anim.enter_enter);
+    }
+
+    /**
+     * @updateTime 2017.2.10
+     * @createAuthor Zou Juequn
+     */
+    public static String getIntentInfo(Intent intent) {
+        StringBuilder builder = new StringBuilder();
+        Bundle bd = intent.getExtras();
+        Set<String> set = bd.keySet();
+        for (String key : set) {
+            builder.append("[");
+            builder.append(key + ":");
+            builder.append(bd.get(key));
+            builder.append("]");
+        }
+        return builder.toString();
+    }
+
+
+    //android获取一个用于打开HTML文件的intent
+    public static void getHtmlFileIntent(Context context,String param) {
+        Uri uri = Uri.parse(param).buildUpon().encodedAuthority("com.android.htmlfileprovider").scheme("content").encodedPath(param).build();
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.setDataAndType(uri, "text/html");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开图片文件的intent
+    public static void getImageFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "image/*");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开PDF文件的intent
+    public static void getPdfFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/pdf");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开文本文件的intent
+    public static void getTextFileIntent(Context context,String param, boolean paramBoolean) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (paramBoolean) {
+            Uri uri1 = Uri.parse(param);
+            intent.setDataAndType(uri1, "text/plain");
+        } else {
+            Uri uri2 = Uri.fromFile(new File(param));
+            intent.setDataAndType(uri2, "text/plain");
+        }
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开音频文件的intent
+    public static void getAudioFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "audio/*");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开视频文件的intent
+    public static void getVideoFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "video/*");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开CHM文件的intent
+    public static void getChmFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/x-chm");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开Word文件的intent
+    public static void getWordFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/msword");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开Excel文件的intent
+    public static void getExcelFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/vnd.ms-excel");
+        context.startActivity(intent);
+    }
+
+    //android获取一个用于打开PPT文件的intent
+    public static void getPptFileIntent(Context context,String param) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        context.startActivity(intent);
+    }
+
 }
